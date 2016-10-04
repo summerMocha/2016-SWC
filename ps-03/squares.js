093056064e9d94da0f2b84cf8d19e5afe4cc47ca
summerMocha <tangwei0326@gmail.com>
2016-10-04 11:27:47 -0400

    ADDDING

---
+++ ps-03/squares.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+    var canvas = document.getElementById('canvas');
+    var drawingPad = canvas.getContext('2d');
+    drawingPad.fillStyle = "hsla(0,100%,10%,1)";
+    drawingPad.fillRect(0,0,200,200);
+    drawingPad.fillRect(205,0,100,100);
+    drawingPad.fillRect(310,0,75,75);
+    drawingPad.fillRect(390,0,50,50);
+    drawingPad.fillRect(445,0,25,25);
+    drawingPad.fillRect(0,220,50,50);
+    drawingPad.fillRect(55,220,100,100);
+    drawingPad.fillRect(0,220,50,50);
+    drawingPad.fillRect(0,275,100,100);
+
+
+    //function myFunction()
+    //{
+     // var x="";
+     // var width=new wide()
+ //if(width<80)
+  //{
+  //  x="pink";
+ // }
+ //else
+  //{
+  // x="yellow"
+
+  //}
+
+  </script>
