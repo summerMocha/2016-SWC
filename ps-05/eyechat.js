889f7589d3d19416a2355b98b0263220fc231f5d
summerMocha <tangwei0326@gmail.com>
2016-10-18 14:01:26 -0400

    PS-05

---
+++ ps-05/eyechat.js
@@ -0,0 +1,171 @@
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "80px Arial";
+ctx.strokeText("E",200,100);
+
+ctx.font = "60px Arial";
+ctx.strokeText("E",100,180);
+
+ctx.font = "60px Arial";
+ctx.strokeText("E",200,180);
+
+
+ctx.font = "60px Arial";
+ctx.strokeText("E",300,180);
+
+
+ctx.font = "40px Arial";
+ctx.strokeText("E",70,260);
+
+
+ctx.font = "40px Arial";
+ctx.strokeText("E",140,260);
+
+
+ctx.font = "40px Arial";
+ctx.strokeText("E",210,260);
+
+
+ctx.font = "40px Arial";
+ctx.strokeText("E",280,260);
+
+
+ctx.font = "40px Arial";
+ctx.strokeText("E",350,260);
+
+
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",50,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",70,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",90,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",110,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",130,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",150,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",170,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",190,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",210,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",230,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",250,340);
+
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",270,340);
+
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",290,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",310,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",330,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",350,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",370,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",390,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",410,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",430,340);
+
+
+
+ctx.font = "20px Arial";
+ctx.strokeText("E",450,340);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
